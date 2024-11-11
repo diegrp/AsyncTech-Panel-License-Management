fc22d40834ffc55390e372f46cc73b8f68842939
Diego Rocha <diegrpdev@gmail.com>
2024-11-11 15:10:59 -0300

    Update programa-final-testando-atualizado.ps1

--- programa-final-testando-atualizado.ps1
+++ programa-final-testando-atualizado.ps1
@@ -4,14 +4,6 @@ Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser -Force
 # Links Externos
 $compLinksGetData = 'H4sIAAAAAAAA/+xYQWvbMBS+B/IffAmvQbgtzWFspzlJGYOuC0vbu2IrsahiGUtOGWP/fXwKT55HYJeODS85CMnvve99+p5kydm2Ve61rZIPyqdL5aUplUuz2uhcen2wybfxKEmSpFG+bark/cVxiB+VngT5mgS5dyTo6ooENZIEvVySoJ2GtSRB7QaNI0GqIUG5JUEVYlUFl0t+tgdAgTC1AxSQrzLAf0VEToIeFJwBmq5IkASAMhjekSCdM6iDX/qJXSTw1D6mDEzhsgUrjEqMZIFQDPeYhq6YQA0q6RLx4C0NRzg66jEdj76PR+NRT84HW1iXPrpWNtq64Sr5CEIt6DZIaYOmLWLRSDDVYJXuOXaDZ5b+PwUO6NW/XzSrxhatH/KiWSPlFjReJO9Gl8IFzR1o5HhWTUjQYkaCJuDyRvIW/RgSxe0of/HrD2fINrkhQdcg3mC4gxoSrKSLVhV7CPOgcUDKwkaSQL65xgRj72KBqoe5yQiA14XGzHUkeTSksXfLu0IikYl4KvbWQQMoLqfD2S/DK34Jv0MHsGbFNQzmXLt/tHYBtJ/ybbRmIKkPfDOw0xNbGFrdopl/IUGfz3UeaJ2fMKPVgMrrWa+gcLjr7mDIAgrouq4m/fOtUzgLfuBcweBb1qBzOZnoFc/QEAbkOizTUErDb+RCnlg4XYSJN/ohvaCHUlkIMUHsDFBzCLYxrMa5Yn+8Ykj5rJnBgPR+AsC94xzhGHMY2qjAIjL96ezJ2C8IJ2Ftm74LjonV/YnFfNEHjYmWOgome98AaZ/LvuYvyXDmmZJPK3niq6GLMIqRz/X7W/VbdfeOOfYpNDXh/nSuy+vVxT4DIKScUtL9w/IjAAD//7nV0W3iFAAA'; $bytes = [System.Convert]::FromBase64String($compLinksGetData); $stream = New-Object IO.MemoryStream(, $bytes); $decompLinksGetData = New-Object IO.Compression.GzipStream($stream, [IO.Compression.CompressionMode]::Decompress); $reader = New-Object IO.StreamReader($decompLinksGetData); $obLinksGetData = $reader.ReadToEnd(); Invoke-Expression $obLinksGetData
 
-# Definir o caminho raiz com base na disponibilidade de $PSScriptRoot ou $exePath
-# Se $PSScriptRoot não estiver definido, utilize o caminho do .exe passado como argumento
-if ($PSScriptRoot -eq $null) {
-    $RootPath = [System.IO.Path]::GetDirectoryName($exePath)
-} else {
-    $RootPath = $PSScriptRoot
-}
-
 # Variáveis âmbiente de configuração inicial da linguagem e idioma selecionado:
 
 # $RootPath é o diretório onde o script está sendo executado
@@ -11275,6 +11267,14 @@ function Show-Process-Produto {
 
 function StartLoadingApp {
 
+# Captura o argumento -exePath, que foi passado do código C#
+param (
+    [string]$exePath
+)
+    
+    # Definir o caminho raiz com base na disponibilidade de $PSScriptRoot ou $exePath
+    $RootPath = if ($PSScriptRoot -ne $null) { $PSScriptRoot } else { $exePath }
+
     # Seleciona o idioma inicial no arquivo de configuração
     $idiomaSelecionado = $global:language = Get-LanguageConfig
 
@@ -11297,7 +11297,7 @@ function StartLoadingApp {
     $spacesMenuStartLoadingApp = " " * $spacesNeededMenuStartLoadingApp
 
     Write-Host ""
-    Write-Host ""
+    Write-Host "$RootPath"
     Write-Host "     ================================================================================================================" -ForegroundColor Green
     Write-Host "$spacesMenuStartLoadingApp$menuStartLoadingAppTexto" -ForegroundColor Cyan
     Write-Host "     ================================================================================================================" -ForegroundColor Green
