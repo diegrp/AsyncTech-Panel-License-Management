18fa0a77c03816ca70b226f14d1937a4e83b7270
Diego Rocha <diegrpdev@gmail.com>
2024-11-12 12:48:43 -0300

    Update programa-final-testando-atualizado.ps1

--- programa-final-testando-atualizado.ps1
+++ programa-final-testando-atualizado.ps1
@@ -11275,8 +11275,7 @@ function Show-Process-Produto {
 function StartLoadingApp {
 
     # Seleciona o idioma inicial no arquivo de configuração
-    $global:language = Get-LanguageConfig
-    $idiomaSelecionado = $global:language
+    $idiomaSelecionado = Get-LanguageConfig
     
     Update-Title-WindowMenu -menuKey "CARREGAMENTO INICIAL PARA VERIFICAÇÃO DE REQUISITOS" -idiomaSelecionado $idiomaSelecionado # Atualiza o título para o menu principal
 
