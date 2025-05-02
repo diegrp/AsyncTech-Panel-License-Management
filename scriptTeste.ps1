50036c0be843cff0b8792b1416cd8ddfb3748804
Diego Rocha <diegrpdev@gmail.com>
2025-05-02 18:48:21 -0300

    Update scriptTeste.ps1

--- scriptTeste.ps1
+++ scriptTeste.ps1
@@ -13,7 +13,7 @@ function Escrever-Etapa {
     # Mostrar no console do PowerShell apenas se não estiver em modo silencioso
     if (-not $ModoSilencioso) {
         $Timestamp = (Get-Date).ToString("HH:mm:ss")
-        Write-Host "$Timestamp - 🔧 $Mensagem"
+        Write-Host "$Timestamp - $Mensagem"
     } else {
         # Sempre envia JSON pela saída padrão (capturado no C#)
         $saida = @{
@@ -39,7 +39,7 @@ function Instalar-Programa {
 
     Escrever-Etapa "Instalação concluída"
 
-    if (-not $ModoSilencioso) {Start-Process calc}
+    if (-not $ModoSilencioso) { Start-Process calc }
 }
 
 function Desinstalar-Programa {
